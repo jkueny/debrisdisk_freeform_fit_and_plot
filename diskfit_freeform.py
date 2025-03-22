cd879d3facebda324b292b6b1c405f66d2b7a113
Jay Kueny <jkueny@email.arizona.edu>
2025-03-22 13:57:26 -0700

    Changing starting model for new HPC run with corrected FM procedure

--- diskfit_freeform.py
+++ diskfit_freeform.py
@@ -653,8 +653,8 @@ if __name__ == "__main__":
     MASK_INDICES = jnp.flatnonzero(MASK)  # 1D indices of nonzero (True) entries
     NUM_FREE = MASK_INDICES.shape[0]
 
-    # STARTING_DISK = fits.getdata("/Users/jkueny/projects/HR4796a_lco2023a_magao-x_20230309_10/raws_20230310T054736_s_lyot_stop/camsci2/lite_psflib/klip_fm_files/camsci2_z_20230309_10_FirstModel.fits")
-    STARTING_DISK = fits.getdata("freeform_run_10500iter.fits") #start from the last run
+    STARTING_DISK = fits.getdata("camsci2_z_20230309_10_FirstModel.fits")
+    # STARTING_DISK = fits.getdata("freeform_run_10500iter.fits") #start from the last run
     STARTING_DISK *= MASK
     INIT_MODEL = jnp.array(STARTING_DISK)
     INIT_MODEL_FLAT = INIT_MODEL.reshape(INIT_MODEL.shape[0] * INIT_MODEL.shape[1])
