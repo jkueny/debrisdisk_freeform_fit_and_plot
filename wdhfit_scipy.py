967c034e87e677c4e571a368bd59008ec13410d5
Jay Kueny <jkueny@email.arizona.edu>
2025-07-21 15:53:29 -0700

    add optax version of L-BFGS in windfit_scipy

--- wdhfit_scipy.py
+++ wdhfit_scipy.py
@@ -126,7 +126,7 @@ def relative_l2(params_all):
 
 
 @partial(jax.jit, static_argnames=["total_pixels", "isRDI"])
-def loss_function(mod_params, x_arr, y_arr, disk_image, aligned_images,
+def loss_function(ravel_params, unravel_params, x_arr, y_arr, disk_image, aligned_images,
                   ref_psfs_stacked, PAs, ref_inds, wdh_PAs,
                   section_inds_arr, klmodes_stacked, evals, evecs_stacked,
                   total_pixels, isRDI, mask2generatehalo, mask_skip_models,
@@ -144,7 +144,7 @@ def loss_function(mod_params, x_arr, y_arr, disk_image, aligned_images,
     Returns:
         Chisquare
     """
-
+    mod_params = unravel_params(ravel_params)
     # DM commands scaled to [0,1] fits cubes do like 10 secs of wall clock time
     # Spatil freq. such that speckles end up at 10 lamb/D
     # So the wind is the rate of change of the phase 2pi v k thing maybe over D
@@ -224,11 +224,10 @@ def optimize_model(target_image, params_init, x_arr, y_arr,
     # Initialize the initial image
     image_params = params_init
     # image_params = initialize_freeform_model_reduced()
-
+    params_ravel, unravel_params = ravel_pytree(image_params) #second return is undo func
     # Set up optimizer, use adaptive stochastic grad descent
-    optimizer = optax.adam(lr)
-    # optimizer = optax.lbfgs()
-    opt_state =  optimizer.init(image_params)
+    optimizer = optax.lbfgs()
+    opt_state =  optimizer.init(params_ravel)
 
     loss_history = []
 
@@ -282,7 +281,7 @@ def optimize_model(target_image, params_init, x_arr, y_arr,
 
     @jax.jit
     def step(image_params, opt_state):
-        loss, grads = loss_and_grad(image_params, x_arr, y_arr,
+        loss, grads = loss_and_grad(params_ravel, unravel_params, x_arr, y_arr,
                                     jax_target_image,
                                     aligned_image_sections, ref_psfs_sections,
                                     position_angles, ref_inds, wdhPAs,
@@ -316,12 +315,12 @@ def optimize_model(target_image, params_init, x_arr, y_arr,
 
 
 def main(config):
-    import matplotlib.pyplot as plt
     # Init the wdh model object
     wdh_obj = ParametricWDH(config)
 
     # Make the intial model
     params_init = wdh_obj.params_init
+
     # Ex. {'ps_global': {'fwhm': 3.0}, 'ps_indiv': [{wdh1_params}, {wdh2_params}, {wdh3_params}]}
     # Make the masks and initial model
     mask2generatehalo, mask2minimize = wdh_obj.prep_binary_masks()
