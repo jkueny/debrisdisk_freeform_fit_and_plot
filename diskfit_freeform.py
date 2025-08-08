531eb85a9a9298101a878bdb1315e94cb7992e98
Jay Kueny <jkueny@email.arizona.edu>
2025-08-07 20:11:40 -0700

    disable the radial scaling on the reference power spectrum, it doesn't help

--- diskfit_freeform.py
+++ diskfit_freeform.py
@@ -377,7 +377,7 @@ def main(config, num_iterations, init_model, reg_lambda, first_time, learning_ra
     reference_model_norm = reference_model_norm - np.mean(reference_model_norm)
     # reference_model_norm += 1.
     psd_ref_model = fft_power_spectrum(reference_model_norm)
-    psd_ref_model *= 1. / (radial_inds**0.25 + 1)
+    # psd_ref_model *= 1. / (radial_inds**0.25 + 1)
 
     # import matplotlib.pyplot as plt
     # plt.imshow(psd_ref_model, vmax=1.)
