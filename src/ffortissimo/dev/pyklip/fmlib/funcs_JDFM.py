0e9fb98562b0797b4b3df3f6065839b28b2739f8
Jay Kueny <jkueny@email.arizona.edu>
2026-01-19 17:40:20 -0700

    create final pipeline script ff_optimize. dry_run CLI toggle added

--- src/ffortissimo/dev/pyklip/fmlib/funcs_JDFM.py
+++ src/ffortissimo/dev/pyklip/fmlib/funcs_JDFM.py
@@ -6,7 +6,7 @@ import jax.numpy as jnp
 import jax.profiler
 
 from functools import partial
-from dev.pyklip.j_klip import rotate_image
+from ffortissimo.dev.pyklip.j_klip import rotate_image
 
 def derotate_and_average(flat_postklip_psfs, PAs, total_pixels, section_inds):
     '''Given (N_images, p_pixels) `flat_postklip_psfs` and (N_images,) PAs
