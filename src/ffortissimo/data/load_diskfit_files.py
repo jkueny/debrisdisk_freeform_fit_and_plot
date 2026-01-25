fix masking issue with the residuals, was an in-place mutation when doing reference model fitting

src/ffortissimo/data/load_diskfit_files.py
@@ -86,7 +86,7 @@ def load_diskfit_components(ffd_obj, init_model=None, new_ref=False):
             f"Mask file not found: {optimization_mask_path}\n"
             "Please run ff_setup.py first to create the masks."
         )
-    optimization_mask_obj = fits.getdata(optimization_mask_path)
+    optimization_mask = fits.getdata(optimization_mask_path)
 
     ffd_obj.allocate_dataset()
     model_firstguess = ffd_obj.get_initial_model(init_model)
@@ -118,7 +118,7 @@ def load_diskfit_components(ffd_obj, init_model=None, new_ref=False):
     print("\n[5/6] Preparing data arrays and indices...")
     total_pixels = np.prod(reduced_data.shape)
     disk_mask = np.array(mask2generatedisk)
-    optimization_mask = np.array(optimization_mask_obj)
+    optimization_mask = np.array(optimization_mask)
     annular_mask = make_annular_mask(disk_mask.shape, 10, ffd_obj.owa)
     disk_mask *= annular_mask
 
@@ -155,11 +155,10 @@ def load_diskfit_components(ffd_obj, init_model=None, new_ref=False):
         "hp_filtersize": hp_filtersize,
         "do_radial_profile_sub": do_radial_profile_sub,
         "do_clean_final_fm": do_clean_final_fm,
-        "optimization_mask_obj": optimization_mask_obj,
         "reference_model_psd": reference_model_psd,
         "window_opt": window_opt,
         "total_pixels": total_pixels,
-        "disk_mask": disk_mask,
+        "disk_mask": disk_mask, #mask2generatedisk * annulus
         "optimization_mask": optimization_mask,
         "disk_mask_indices": disk_mask_indices,
         "optimization_mask_indices": optimization_mask_indices,

src/ffortissimo/data/save_diskfit_results.py
@@ -281,7 +281,7 @@ def harness_optimized_model(optimized_params, ffd_obj, reduced_data, psf, total_
 
     print("Calculating residuals between reduced data and optimized forward model...")
     residuals = np.asarray(reduced_data - optimized_fm_rprofsub)
-    residuals_roi = residuals * optimization_mask
+    residuals_roi = residuals.copy() * optimization_mask
     residuals_roi[residuals_roi == 0.] = np.nan
     residuals_roi = residuals_roi / noise_reconstructed
 

src/ffortissimo/modeling/disk_freeform.py
@@ -361,11 +361,13 @@ class FreeFormDisk:
         print(f"Iteration {self.iteration}: {params}")
         self.iteration += 1
     def fit_reference_model(self, noise_map, reduced_data):
-        reduced_data[reduced_data != reduced_data] = 0.
-        reduced_data *= self.mask2generatedisk
-        self.data = reduced_data
-        noise_map[noise_map != noise_map] = 1.
-        self.noise_map = noise_map
+        reduced_data_local = np.array(reduced_data, copy=True)
+        reduced_data_local[reduced_data_local != reduced_data_local] = 0.
+        reduced_data_local *= self.mask2generatedisk
+        self.data = reduced_data_local
+        noise_map_local = np.array(noise_map, copy=True)
+        noise_map_local[noise_map_local != noise_map_local] = 1.
+        self.noise_map = noise_map_local
         if self.params_file["OPTIMIZE_REFERENCE"]:
             reference_model = self.fit_simple_disk_model()
         else: