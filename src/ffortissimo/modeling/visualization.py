dcc5ae050348660c37e27781d844c603a7f203e8
Joseph Long <github@joseph-long.com>
2026-04-29 22:22:59 -0400

    Use 99.9 pctl for vmax model

--- src/ffortissimo/modeling/visualization.py
+++ src/ffortissimo/modeling/visualization.py
@@ -21,7 +21,7 @@ def plot_training(out_filename, reduced_data, freeform_fm_full, full_model_image
     data_vmax = np.percentile(reduced_data[mask_good], max_percent)
     data_vmin = -data_vmax
     # model_vmin, model_vmax = 0, np.percentile(full_model_image[mask_good], max_percent)
-    model_max_val = np.max(full_model_image[mask_good])
+    model_max_val = np.percentile(full_model_image[mask_good], 99.9)
     model_vmin, model_vmax = 0, max(ref_disk_median + 3 * ref_disk_std, model_max_val)
     data_space_norm = simple_norm(reduced_data, 'linear', vmin=data_vmin, vmax=data_vmax)
     reduced_data_masked = np.array(reduced_data)
